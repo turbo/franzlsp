21388f0ae72ea0a7f0988f736d5841172b35cbf8
turbo <minxomat@gmail.com>
2019-05-20 10:53:28 +0200

    iter script

--- lisp-setup.sh
+++ lisp-setup.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+# clean old files
+rm -rf /usr/local
+
+
+# link lisp library
+mkdir -p /usr/local/lib
+ln -s "$(pwd)/src/lisplib" /usr/local/lib/lisp
+
+# arm binaries
+chmod u+x "$(pwd)/bin/"*
+
+# I'm unclear as to where the include files should live. Adding an -I to CC will not work, it's not enough to get liszt to care about them :/
+
+export PATH="${PATH};$(pwd)/bin"
+
+# rebuild libraries
+# - bin in path
+# - cd to lisplib
